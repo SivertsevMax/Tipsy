b7c8201c869079d4469acfac612c76e60e6b95c5
Sivertsev Maxim <sivertsev1995@icloud.com>
2023-02-24 10:23:59 +0300

    1

--- Tipsy/Controller/CalculteViewController.swift
+++ Tipsy/Controller/CalculteViewController.swift
@@ -1,26 +1,46 @@
 import UIKit
 
 class CalculateViewController: UIViewController {
+    
+    var percentForSplit = 0
+    var splitTimes = 2
+    var billTotal = 0
 
-    @IBOutlet weak var peopleSelected: UILabel!
+    @IBOutlet weak var chooseSplit: UILabel!
     @IBOutlet weak var payValue: UITextField!
+    @IBOutlet var selectTip: [UIButton]!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
-    @IBAction func showResultButtonPressed(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "show result", sender: self)
+    @IBAction func payValueEnter(_ sender: UITextField) {
+        if let billTotal = sender.text {
+            self.billTotal = Int(billTotal) ?? 0
+        }
+        print(billTotal)
     }
     
-    @IBAction func selectPercent(_ sender: UIButton) {
+    @IBAction func showResultButtonPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "show result", sender: self)
+        
+    }
+    
+    @IBAction func selectTipPressed(_ sender: UIButton) {
+        self.percentForSplit = Int(sender.restorationIdentifier ?? "0") ?? 0
+        selectTip[0].isSelected = false
+        selectTip[1].isSelected = false
+        selectTip[2].isSelected = false
+        sender.isSelected = true
+        
     }
     
     @IBAction func selectPeople(_ sender: UIStepper) {
+        chooseSplit.text = "\(Int(sender.value))"
+        splitTimes = Int(sender.value)
+        
     }
-    
-    
-    
-    
 }
 
